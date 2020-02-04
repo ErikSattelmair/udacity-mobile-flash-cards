312a711d4951473822b3214d13ed7ffcbe7e3042
erik <erik.sattelmair@gmx.de>
2020-02-04 11:59:40 +0100

    .gitignore added

---
+++ components/QuizCard.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+class QuizzCard extends Component {
+
+    //TODO: implement me!
+
+    render() {
+        return (
+            <View>
+                <Text>Hallo QuizzCard</Text>
+            </View>
+        )
+    }
+}
+
+export default QuizzCard
